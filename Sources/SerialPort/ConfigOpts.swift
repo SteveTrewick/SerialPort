2c2c2b7ef6ad25121f59f8dcdc49bc048d4bb50d
Steve Trewick <steve.trewick@icloud.com>
2024-01-19 17:14:44 +0000

    oof

---
+++ Sources/SerialPort/ConfigOpts.swift
@@ -0,0 +1,122 @@
+import Foundation
+
+/*
+  Serial port config options
+  
+  Please note that this is *not* complete and may never be,
+  options will be added as I (or indeed you, dear reader!) need them
+ 
+  Probably the next set we *really* need are the CRLF translations,
+  though I have a feeling they may not work anyway in raw mode.
+  
+*/
+
+
+// well, this is much stupider than it needs to be
+// these consts are imported, but we can (AFAIK) only use these values
+// and we have to use literals for our enum initialisation, not consts
+
+// still, this way we get a caseiterable that we can use in a list
+// and better autocomplete, copy copy, pasty pasty.
+
+public enum BaudRate : CaseIterable {
+    case baud_0
+    case baud_50
+    case baud_75
+    case baud_110
+    case baud_134
+    case baud_150
+    case baud_200
+    case baud_300
+    case baud_600
+    case baud_1200
+    case baud_1800
+    case baud_2400
+    case baud_4800
+    case baud_7200
+    case baud_9600
+    case baud_19200
+    case baud_38400
+    case baud_14400
+    case baud_28800
+    case baud_57600
+    case baud_76800
+    case baud_115200
+    case baud_230400
+  
+  
+    public var speed : speed_t {
+        switch self {
+            case .baud_0      : return speed_t(B0)
+            case .baud_50     : return speed_t(B50)
+            case .baud_75     : return speed_t(B75)
+            case .baud_110    : return speed_t(B110)
+            case .baud_134    : return speed_t(B134)
+            case .baud_150    : return speed_t(B150)
+            case .baud_200    : return speed_t(B200)
+            case .baud_300    : return speed_t(B300)
+            case .baud_600    : return speed_t(B600)
+            case .baud_1200   : return speed_t(B1200)
+            case .baud_1800   : return speed_t(B1800)
+            case .baud_2400   : return speed_t(B2400)
+            case .baud_4800   : return speed_t(B4800)
+            case .baud_7200   : return speed_t(B7200)
+            case .baud_9600   : return speed_t(B9600)
+            case .baud_19200  : return speed_t(B19200)
+            case .baud_38400  : return speed_t(B38400)
+            case .baud_14400  : return speed_t(B14400)
+            case .baud_28800  : return speed_t(B28800)
+            case .baud_57600  : return speed_t(B57600)
+            case .baud_76800  : return speed_t(B76800)
+            case .baud_115200 : return speed_t(B115200)
+            case .baud_230400 : return speed_t(B230400)
+        }
+    }
+  
+}
+
+// same oh, same oh, in fact, there's going to a bit more of this as we go innit?
+
+public enum DataBits : Int32, CaseIterable {
+  
+  case five
+  case six
+  case seven
+  case eight
+  
+  public var count : UInt {
+    switch self {
+      case .five  : return UInt(CS5)
+      case .six   : return UInt(CS6)
+      case .seven : return UInt(CS7)
+      case .eight : return UInt(CS8)
+    }
+  }
+}
+
+// this one is trickier to set, combo of bits
+public enum Parity : CaseIterable {
+  case none
+  case even
+  case odd
+}
+
+// this one is a set or unset jobby, so no raw values
+public enum StopBits : CaseIterable {
+  case one
+  case two
+}
+
+public enum LineMode {
+  case canonical, raw
+}
+
+// now we bundle them up into here
+
+public struct SerialConfig {
+  var baud     : BaudRate = .baud_9600   // default 9600 8N1, as is tradition
+  var databits : DataBits = .eight
+  var parity   : Parity   = .none
+  var stopbits : StopBits = .one
+  var linemode : LineMode = .raw
+}
--- Sources/SerialPort/SerialPort.swift
+++ Sources/SerialPort/SerialPort.swift
@@ -1,6 +1,122 @@
-public struct SerialPort {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import PosixInputStream
 
-    public init() {
+
+ 
+
+public class TTY {
+  
+  public  let descriptor  : Int32
+  private let operationQ = DispatchQueue(label: "SerialPortOpQ")
+  
+  private(set) public var options      = termios()
+                      var origoptions  = termios()
+  
+  
+  public let stream : PosixInputStream  // yes, yes I did just expose that as API.
+  
+  
+  public init(descriptor: Int32) {
+    
+    self.descriptor = descriptor
+    
+    tcgetattr(descriptor, &origoptions)
+    tcgetattr(descriptor, &options)
+    
+    stream = PosixInputStream(descriptor: descriptor, targetQueue: operationQ)
+  }
+  
+  
+  
+  public func configure(config: SerialConfig) {
+    
+    cfsetspeed(&options, config.baud.speed)
+    
+    
+    options.c_cflag &= ~UInt(CSIZE)
+    options.c_cflag |= config.databits.count
+    
+    
+    switch config.parity {
+      case .none :
+        options.c_cflag &= ~UInt(PARENB)
+        
+      case .even :
+        options.c_cflag |=  UInt(PARENB)
+        options.c_cflag &= ~UInt(PARODD)
+                   
+      case .odd  :
+        options.c_cflag |= UInt(PARENB)
+        options.c_cflag |= UInt(PARODD)
     }
+    
+    
+    switch config.stopbits {
+      case .one : options.c_cflag &= ~UInt(CSTOPB)
+      case .two : options.c_cflag |=  UInt(CSTOPB)
+    }
+    
+    
+    switch config.linemode {
+      case .canonical : options.c_lflag |=  ( UInt(ICANON) | UInt(ECHO) | UInt(ECHOE) )
+      case .raw       : options.c_lflag &= ~( UInt(ICANON) | UInt(ECHO) | UInt(ECHOE) | UInt(ISIG) )
+    }
+    
+    // always, don't @ me : https://www.ing.iac.es/~docs/external/serial/serial.pdf
+    options.c_cflag |= tcflag_t(CREAD | CLOCAL)
+    
+    
+    tcsetattr(descriptor, TCSANOW, &options)
+  }
+  
+  
+  // until we add the rest of the config opts, we can bounce through
+  // termios, but if you find yourself doing this, you should be adding
+  // proper config opts. m'kay?
+  
+  public func ocnfigure(options: termios) {
+    var options = options
+    tcsetattr(descriptor, TCSANOW, &options)
+  }
+  
+  
+  
+  public func close() {
+    
+    self.reset()              // just in case
+    
+    Darwin.close(descriptor)  // technically this returns a vaule, but there's
+                              // SFA we can do about it if it doesn't work. close more?
+  }
+  
+  
+  
+  // reset by applying the original setting that the port came with
+  
+  public func reset() {
+    tcsetattr(descriptor, TCSANOW, &origoptions)
+  }
+  
+  
+  
+  // send data, duh, apply a completion hamdelr if you need to know why its not working,
+  
+  public func send( data: Data, complete: ((Result<Int, Trace>) -> Void)? = nil ) {
+    
+    operationQ.async { [self] in
+      
+      data.withUnsafeBytes {
+        
+        let wrote = write(descriptor, $0.baseAddress, data.count)
+        
+        if wrote == -1 {
+            complete?( .failure( .posix(self, tag: "serial write") ) )
+        }
+        else { complete?(.success(wrote)) }
+      }
+    }
+  }
+  
+  
 }
+
